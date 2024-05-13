26deb2965259b7d0932683b323d109e82ed099d6
Sunjay E. Dixit <jaydixit.work@gmail.com>
2024-05-13 15:59:05 -0700

    Update org-transclusion-sandbox.el

--- org-transclusion-sandbox.el
+++ org-transclusion-sandbox.el
@@ -1,5 +1,5 @@
 
-(defun return-current-org-heading-plus-one ()
+(defun insert-current-org-heading-level-plus-one ()
   "Return the current org-heading level plus one as an integer.
 If the point is before the first heading, return 1.
 Insert the level into the buffer after the word \":level \"."
@@ -11,9 +11,8 @@ Insert the level into the buffer after the word \":level \"."
     (insert (format " :level %d" level))
     level))
 
-
 (defun tr-insert-transclusion-match-heading-level ()
-"Insert a transclusion org-roam link at the appropriate heading level."
+  "Insert a transclusion org-roam link at the appropriate heading level."
   (interactive)
   ;; Step 1: Call org-roam-node-insert and capture the inserted link
   (let ((link (org-roam-node-insert nil t)))  ; 't' returns the description instead of inserting
@@ -22,8 +21,7 @@ Insert the level into the buffer after the word \":level \"."
     (insert (format "#+transclude: %s" link))
     ;; Step 3: Move to the end of the line
     (end-of-line)
-    ;; Step 5: Add ':level x' where x is the current org-heading level plus one
-          (return-current-org-heading-plus-one)
-          ))
-  ;; Ensure formatting is proper, move back to the heading
+    ;; Step 4: Add ':level x' where x is the current org-heading level plus one
+    (insert-current-org-heading-level-plus-one))
+  ;; Step 5: Ensure formatting is proper, move back to the heading
   (org-back-to-heading))
