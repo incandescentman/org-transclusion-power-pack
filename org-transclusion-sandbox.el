050a1e3f4e745b8b7d940cbd3985d4d5ca566fe7
Sunjay E. Dixit <jaydixit.work@gmail.com>
2024-05-13 15:56:41 -0700

    Update org-transclusion-sandbox.el

--- org-transclusion-sandbox.el
+++ org-transclusion-sandbox.el
@@ -13,17 +13,17 @@ Insert the level into the buffer after the word \":level \"."
 
 
 (defun tr-insert-transclusion-match-heading-level ()
-  "Insert a transclusion org-roam link at the appropriate heading level."
+"Insert a transclusion org-roam link at the appropriate heading level."
   (interactive)
   ;; Step 1: Call org-roam-node-insert and capture the inserted link
   (let ((link (org-roam-node-insert nil t)))  ; 't' returns the description instead of inserting
     ;; Step 2: Add '#+transclude:' at the beginning of the current line
     (beginning-of-line)
     (insert (format "#+transclude: %s" link))
-;; Step 3: Move to the end of the line
-      (end-of-line)
-      ;; Step 5: Add ':level x' where x is the current org-heading level plus one
-                    (return-current-org-heading-plus-one)
-              )))
+    ;; Step 3: Move to the end of the line
+    (end-of-line)
+    ;; Step 5: Add ':level x' where x is the current org-heading level plus one
+          (return-current-org-heading-plus-one)
+          ))
   ;; Ensure formatting is proper, move back to the heading
-  (org-back-to-heading)))
+  (org-back-to-heading))
